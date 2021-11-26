bfa83eea13f19ebde57f8e2ef349858bbd9a46fb
pencil-user <57478498+pencil-user@users.noreply.github.com>
2021-11-26 06:13:57 +0100

    update

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -1,7 +1,8 @@
 function cors(req, res, next) {
     res.header("Access-Control-Allow-Origin", "https://dusan-forum-frontend.herokuapp.com")
     //res.header("Access-Control-Allow-Origin", "http://localhost:3000/")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    res.header("Access-Control-Allow-Headers", "*")
     next()
 }
 
