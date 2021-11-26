b7cb3eb3617a155c8dc3e881bda3a8ad6bf70623
pencil-user <57478498+pencil-user@users.noreply.github.com>
2021-11-26 06:34:25 +0100

    update

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -6,6 +6,8 @@ function cors(req, res, next) {
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    res.header("Access-Control-Expose-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+
     next()
 }
 
