019b766e7020be61b6b9245ebfb7ba8224656e3b
pencil-user <57478498+pencil-user@users.noreply.github.com>
2021-11-30 05:31:14 +0100

    update

--- src/routes/threads.js
+++ src/routes/threads.js
@@ -90,9 +90,9 @@ router.patch('/:id', auth(USER_LEVEL.USER),
 
         if (req._user.level < USER_LEVEL.ADMIN) // admin can alter any thread, ordinary users can only alter their own
         {
-            let posts = await threadService.getThreadById(id)
+            let thread = await threadService.getThreadById(id)
 
-            if (posts[0].created_by_id !== req._user.id) {
+            if (thread.created_by_id !== req._user.id) {
                 res.status(401).send({ error: 'access denied' })
                 return;
             }
